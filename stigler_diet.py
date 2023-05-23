63d77f2a4cd792c56e266cc2b2c64d3ce6aa909a
Lucas Emilio <lucasemilio.mf@gmail.com>
2023-05-23 18:05:37 -0300

    Update stigler_diet.py

--- stigler_diet.py
+++ stigler_diet.py
@@ -5,6 +5,9 @@ from charles.selection import fps, tournament_sel
 from charles.mutation import swap_mutation, inversion_mutation
 from charles.crossover import single_point_co, cycle_xo, pmx, arithmetic_xo
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 
 def get_values(individual):
@@ -83,21 +86,28 @@ def get_neighbours(self):
 Individual.get_fitness = get_fitness
 Individual.get_neighbours = get_neighbours
 
-
 def execute_ga(selection, xo, mutation):
     pop = Population(size=300, optim="min", sol_size=len(data), valid_set=[0, 1, 2, 3], replacement=True)
+    fitness_stats = {'min': [], 'max': [], 'avg': []}
 
-    pop.evolve(gens=100,
-               xo_prob=0.9,
-               mut_prob=0.1,
-               select=selection,
-               mutate=mutation,
-               crossover=xo,
-               elitism=True)
+    for gen in range(100):
+        pop.evolve(
+            gens=1,
+            xo_prob=0.9,
+            mut_prob=0.1,
+            select=selection,
+            mutate=mutation,
+            crossover=xo,
+            elitism=True)
+
+        # Get fitness stats for this generation
+        fitness_values = [ind.get_fitness() for ind in pop.individuals]
+        fitness_stats['min'].append(min(fitness_values))
+        fitness_stats['max'].append(max(fitness_values))
+        fitness_stats['avg'].append(np.mean(fitness_values))
 
     best_individual = pop.get_elite()
-    return best_individual.get_fitness()
-
+    return best_individual.get_fitness(), fitness_stats
 
 selection_methods = [fps, tournament_sel]
 xo_methods = [single_point_co, arithmetic_xo]
@@ -105,22 +115,29 @@ mutation_methods = [swap_mutation, inversion_mutation]
 
 results_df = pd.DataFrame(columns=['selection_method', 'mutation_method', 'crossover_method', 'average_fitness'])
 
+fitness_data = {}
+
 for selection in selection_methods:
     for xo in xo_methods:
         for mutation in mutation_methods:
             avg_fitness = 0
             executions = 30
             for i in range(executions):
-                fitness = execute_ga(selection, xo, mutation)
-                avg_fitness+= fitness
-            avg_fitness = avg_fitness/executions
+                fitness, fitness_stats = execute_ga(selection, xo, mutation)
+                avg_fitness += fitness
+
+                # Add fitness_stats to fitness_data
+                key = (selection.__name__, xo.__name__, mutation.__name__)
+                if key not in fitness_data:
+                    fitness_data[key] = {'min': [], 'max': [], 'avg': []}
+                fitness_data[key]['min'].extend(fitness_stats['min'])
+                fitness_data[key]['max'].extend(fitness_stats['max'])
+                fitness_data[key]['avg'].extend(fitness_stats['avg'])
+
+            avg_fitness = avg_fitness / executions
             row = {'selection_method': selection.__name__, 'mutation_method': mutation.__name__,
-                    'crossover_method': xo.__name__, 'average_fitness': avg_fitness}
+                   'crossover_method': xo.__name__, 'average_fitness': avg_fitness}
             results_df = pd.concat([results_df, pd.DataFrame(row, index=[0])], ignore_index=True)
-
-
-results_df.to_csv('output.csv', index=False)
-
 
 #for index, bit in enumerate(elite.representation):
 #    if bit != 0:
@@ -132,3 +149,93 @@ results_df.to_csv('output.csv', index=False)
 #    print(f"Consumed {list_values_elite[i + 1]} {nutrients[i][0]} in a year (Required {nutrients[i][1] * 365})")
 
 #print(f"Cost was ${elite.get_fitness():.2f}")
+
+
+
+# Sort the dataframe by average_fitness in ascending order (lowest values first) 
+results_df = results_df.sort_values(by='average_fitness') 
+
+results_df.to_csv('output2.csv', index=False)
+
+# Get the top 3 combinations
+
+top_combinations_df = results_df[:3]
+
+#top_combinations_df
+
+
+# Plot Average Fitness Over Generations for Top 3 Combinations
+
+# Reset index for top_combinations_df
+top_combinations_df = top_combinations_df.reset_index(drop=True)
+
+fig, ax = plt.subplots()
+colors = ['blue', 'green', 'red']
+
+for i, row in top_combinations_df.iterrows():
+    selection_method = row['selection_method']
+    crossover_method = row['crossover_method']
+    mutation_method = row['mutation_method']
+    key = (selection_method, crossover_method, mutation_method)
+
+    # Extract the corresponding fitness data
+    values = fitness_data[key]
+
+    # Get generation range
+    generations = range(100)  # 100 generations
+
+    # Plot average fitness over generations
+    ax.plot(generations, values['avg'][:100], label=f'Average for {key[0]}, {key[1]}, {key[2]}', color=colors[i])
+    ax.fill_between(generations, values['min'][:100], values['max'][:100], color=colors[i], alpha=0.1)
+
+# Add labels and legend
+ax.set_xlabel('Generation')
+ax.set_ylabel('Fitness')
+ax.legend()
+
+# Set title
+ax.set_title('Average Fitness Over Generations for Top 3 Combinations')
+
+plt.tight_layout()
+plt.show()
+
+
+#### 3 separate plots
+
+# Reset index for top_combinations_df
+top_combinations_df = top_combinations_df.reset_index(drop=True)
+
+colors = ['blue', 'green', 'red']
+
+fig, axs = plt.subplots(1, 3, figsize=(20, 5), sharey=True)  # Create a 1x3 grid of subplots with shared y-axis
+
+for i, row in top_combinations_df.iterrows():
+    selection_method = row['selection_method']
+    crossover_method = row['crossover_method']
+    mutation_method = row['mutation_method']
+    key = (selection_method, crossover_method, mutation_method)
+
+    # Extract the corresponding fitness data
+    values = fitness_data[key]
+
+    # Get generation range
+    generations = range(100)  # 100 generations
+
+    # Plot average fitness over generations in the corresponding subplot
+    ax = axs[i]
+    ax.plot(generations, values['avg'][:100], label=f'Average for {key[0]}, {key[1]}, {key[2]}', color=colors[i])
+    ax.fill_between(generations, values['min'][:100], values['max'][:100], color=colors[i], alpha=0.1)
+
+    # Add labels and legend
+    ax.set_xlabel('Generation')
+    ax.set_ylabel('Fitness')
+    ax.legend()
+
+    # Set title for each subplot
+    #ax.set_title(f'Average Fitness Over Generations\n{key[0]}, {key[1]}, {key[2]}')
+
+# Add a single title above all three subplots
+plt.suptitle('Average Fitness Over Generations for Top 3 Combinations', fontsize=16)
+
+plt.tight_layout()
+plt.show()
