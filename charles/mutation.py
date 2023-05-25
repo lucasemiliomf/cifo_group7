d743b811216caa1a8b61c83ff605dea40e9cf647
Lucas Emilio <lucasemilio.mf@gmail.com>
2023-05-25 16:28:51 -0300

    Update mutation.py

--- charles/mutation.py
+++ charles/mutation.py
@@ -1,28 +1,4 @@
-from random import randint, sample
-
-
-def binary_mutation(individual):
-    """Binary mutation for a GA individual. Flips the bits.
-
-    Args:
-        individual (Individual): A GA individual from charles.py
-
-    Raises:
-        Exception: When individual is not binary encoded.py
-
-    Returns:
-        Individual: Mutated Individual
-    """
-    mut_index = randint(0, len(individual) - 1)
-
-    if individual[mut_index] == 0:
-        individual[mut_index] = 1
-    elif individual[mut_index] == 1:
-        individual[mut_index] = 0
-    else:
-        raise Exception(
-            f"Trying to do binary mutation on {individual}. But it's not binary.")
-    return individual
+from random import sample
 
 
 def swap_mutation(individual):
@@ -49,7 +25,6 @@ def inversion_mutation(individual):
         Individual: Mutated Individual
     """
     mut_indexes = sample(range(0, len(individual)), 2)
-    #mut_indexes = [0,3]
     mut_indexes.sort()
     individual[mut_indexes[0]:mut_indexes[1]] = individual[mut_indexes[0]:mut_indexes[1]][::-1]
     return individual
@@ -59,19 +34,3 @@ if __name__ == '__main__':
     test = [1, 2, 3, 4, 5, 6]
     test = inversion_mutation(test)
     print(test)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
