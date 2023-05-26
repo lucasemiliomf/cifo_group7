46a3b706fb6384db03d929192f98ca6dceec61b8
Lucas Emilio <lucasemilio.mf@gmail.com>
2023-05-26 18:18:29 -0300

    Update mutation.py

--- charles/mutation.py
+++ charles/mutation.py
@@ -1,4 +1,32 @@
-from random import sample
+from random import sample, randint
+
+
+def quaternary_mutation(individual):
+    """Binary mutation for a GA individual. Flips the bits.
+
+    Args:
+        individual (Individual): A GA individual from charles.py
+
+    Raises:
+        Exception: When individual is not binary encoded.py
+
+    Returns:
+        Individual: Mutated Individual
+    """
+    mut_index = randint(0, len(individual) - 1)
+
+    if individual[mut_index] == 0:
+        individual[mut_index] = sample([1, 2, 3], 1)[0]
+    elif individual[mut_index] == 1:
+        individual[mut_index] = sample([0, 2, 3], 1)[0]
+    elif individual[mut_index] == 2:
+        individual[mut_index] = sample([0, 1, 3], 1)[0]
+    elif individual[mut_index] == 3:
+        individual[mut_index] = sample([0, 1, 2], 1)[0]
+    else:
+        raise Exception(
+            f"Trying to do a quaternary mutation on {individual}. But it's not quaternary.")
+    return individual
 
 
 def swap_mutation(individual):
